2ec2ff76566bf1f2e1316a188e81990c28f82a85
kadomatsu <tktkcap.com@gmail.com>
2022-11-10 22:12:54 +0900

    コントローラーの情報を取れるようになった（途中）

---
+++ Assets/Script/Tools/ControllerStateWatcher.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+namespace VrKokusaitenjijo.Tools
+{
+    /// <summary>
+    /// XRIT用のControllerState監視スクリプト
+    /// </summary>
+    [RequireComponent(typeof(XRBaseController))]
+    
+    public class ControllerStateWatcher : MonoBehaviour
+    {
+        [SerializeField] private XRBaseController _leftController;
+        [SerializeField] private XRBaseController _rightController;
+
+        private XRControllerState _leftControllerState;
+        private XRControllerState _rightControllerState;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            _leftControllerState = _leftController.currentControllerState;
+            _rightControllerState = _rightController.currentControllerState;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log(_leftControllerState.ToString());
+            Debug.Log(_rightControllerState.ToString());
+
+        }
+    }
+}
